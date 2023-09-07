a6cd8cfbe8db409cb7e665905d6728fe54bc12a5
Arash <45104106+arashHaratian@users.noreply.github.com>
2023-09-08 01:12:03 +0200

    add dataset

--- draft.py
+++ draft.py
@@ -1,5 +1,10 @@
 import torch
+import torchvision.datasets as torch_datasets
+
 
 test = torch.hub.load('facebookresearch/dino:main', 'dino_resnet50')
 vicreg = torch.hub.load('facebookresearch/vicreg:main', 'resnet50')
 test = torch.load("dinov2_vits14_linear4_head.pth")
+
+
+torch_datasets.CelebA("./dataset", download=True) # if not working, try download files manually off the internet
