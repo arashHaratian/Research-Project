40c7cc95ec5bba4bdc21244e6811b37e293d8ba3
Arash <45104106+arashHaratian@users.noreply.github.com>
2023-09-08 14:27:53 +0200

    simple run of algorithms on data

--- draft.py
+++ draft.py
@@ -1,10 +1,32 @@
 import torch
 import torchvision.datasets as torch_datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+from matplotlib import pyplot as plt
+import torch.nn.functional as F
 
 
-test = torch.hub.load('facebookresearch/dino:main', 'dino_resnet50')
+dino = torch.hub.load('facebookresearch/dino:main', 'dino_resnet50')
 vicreg = torch.hub.load('facebookresearch/vicreg:main', 'resnet50')
-test = torch.load("dinov2_vits14_linear4_head.pth")
+# dinov2 =torch.hub.load('facebookresearch/dinov2', 'dinov2_vitg14')
+# dinov2 =torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+dinov2.eval()
+dinov2.cuda()
+# dinov2 = torch.load("dinov2_vits14_linear4_head.pth")
 
 
-torch_datasets.CelebA("./dataset", download=True) # if not working, try download files manually off the internet
+
+data = torch_datasets.CelebA("./dataset", download=True, transform=ToTensor()) # if not working, try download files manually off the internet (mostly works in the morning XD)
+data = DataLoader(data)
+sample_data = next(iter(data))[0]
+# plt.imshow(sample_data.squeeze(0).permute(1, 2, 0))
+# plt.show()
+# plt.close()
+
+
+# vicreg(sample_data)
+# dino(sample_data)
+
+padded_sample_data = F.pad(sample_data)
+padded_sample_data = F.pad(sample_data, (2, 2, 3, 3)).to("cuda") # Images should be multiple of 14
+dinov2(padded_sample_data)
