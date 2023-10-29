7c324f71b5bfd45133515580c8a2de0bd34333ac
Arash <45104106+arashHaratian@users.noreply.github.com>
2023-10-29 11:37:48 +0100

    Update colloect_data.sh

--- colloect_data.sh
+++ colloect_data.sh
@@ -1,27 +1,50 @@
 # !/bin/sh
 
-
-
+# Single target attr (gender) with vicreg
+echo "--- run 1"
 python pipeline.py --seed "1" --model "vicreg" --target-idx "20"
+echo "--- run 2"
 python pipeline.py --seed "2" --model "vicreg" --target-idx "20"
+echo "--- run 3"
 python pipeline.py --seed "3" --model "vicreg" --target-idx "20"
+echo "--- run 4"
 python pipeline.py --seed "4" --model "vicreg" --target-idx "20"
+echo "--- run 5"
 python pipeline.py --seed "5" --model "vicreg" --target-idx "20"
 
+# Multiple target attr with vicreg
+echo "--- run 6"
 python pipeline.py --seed "1" --model "vicreg" --target-idx "3, 7, 13, 20"
+echo "--- run 7"
 python pipeline.py --seed "2" --model "vicreg" --target-idx "3, 7, 13, 20"
+echo "--- run 8"
 python pipeline.py --seed "3" --model "vicreg" --target-idx "3, 7, 13, 20"
+echo "--- run 9"
 python pipeline.py --seed "4" --model "vicreg" --target-idx "3, 7, 13, 20"
+echo "--- run 10"
 python pipeline.py --seed "5" --model "vicreg" --target-idx "3, 7, 13, 20"
 
+
+# Single target attr (gender) with dino
+echo "--- run 11"
 python pipeline.py --seed "1" --model "dino" --target-idx "20"
+echo "--- run 12"
 python pipeline.py --seed "2" --model "dino" --target-idx "20"
+echo "--- run 13"
 python pipeline.py --seed "3" --model "dino" --target-idx "20"
+echo "--- run 14"
 python pipeline.py --seed "4" --model "dino" --target-idx "20"
+echo "--- run 15"
 python pipeline.py --seed "5" --model "dino" --target-idx "20"
 
+# Multiple target attr with dino
+echo "--- run 16"
 python pipeline.py --seed "1" --model "dino" --target-idx "3, 7, 13, 20"
+echo "--- run 17"
 python pipeline.py --seed "2" --model "dino" --target-idx "3, 7, 13, 20"
+echo "--- run 18"
 python pipeline.py --seed "3" --model "dino" --target-idx "3, 7, 13, 20"
+echo "--- run 19"
 python pipeline.py --seed "4" --model "dino" --target-idx "3, 7, 13, 20"
+echo "--- run 20"
 python pipeline.py --seed "5" --model "dino" --target-idx "3, 7, 13, 20"
